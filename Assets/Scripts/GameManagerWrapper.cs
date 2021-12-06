273388b5851b9c699fa2d363d8b3f1feca00802e
Sagie Levy <sagie@lightricks.com>
2021-12-06 13:18:11 +0200

    manage game state correctly. save items between scenes

---
+++ Assets/Scripts/GameManagerWrapper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using DevionGames.InventorySystem;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class GameManagerWrapper : MonoBehaviour
+{
+    private static GameStateManager Instance;
+
+    [SerializeField] private ItemDatabase itemDatabase;
+    [SerializeField] private ItemCollection inventoryItemCollection;
+    [SerializeField] private FilterController filterController;
+
+    [SerializeField] private GameWinChecker gameWinChecker;
+    [SerializeField] private TMPro.TextMeshProUGUI instructionsText;
+
+    public static GameStateManager GetInstance { get { return Instance; } }
+
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = new GameStateManager(itemDatabase, filterController,
+                gameWinChecker, instructionsText, inventoryItemCollection);
+        } else
+        {
+            GetInstance.StartLevel(filterController, gameWinChecker,
+                instructionsText, inventoryItemCollection);
+
+            ItemContainer.AddItems("Inventory", inventoryItemCollection.ToArray());
+        }
+    }
+
+    public void MoveToRoomScene(string roomName)
+    {
+        SceneManager.LoadScene(roomName);
+    }
+}
---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevionGames.InventorySystem;
+using UnityEngine;
+
+public class GameStateManager
+{
+    private const int itemCount = 4;
+
+    private List<string> levelData;
+    private ItemCollection previousRoomItemCollection;
+
+    public GameStateManager(ItemDatabase itemDatabase, FilterController filterController,
+        GameWinChecker gameWinChecker, TMPro.TextMeshProUGUI instructionsText,
+        ItemCollection roomItemCollection)
+    {
+        PlayerPrefs.DeleteAll();
+        previousRoomItemCollection = roomItemCollection;
+
+        var levelGenerator = new LevelGenerator();
+
+        var itemNames = itemDatabase.items.Select(x => x.name).ToList();
+
+        levelData = levelGenerator.GenerateLevel(itemNames, itemCount);
+
+        gameWinChecker.SetLevelData(levelData.ToHashSet());
+        instructionsText.text = new LevelGenerator().GenerateLevelText(levelData);
+
+        filterController.SetColorlessFactor(0);
+        filterController.SetClearColor();
+    }
+
+    public void StartLevel(FilterController filterController,
+        GameWinChecker gameWinChecker, TMPro.TextMeshProUGUI instructionsText,
+        ItemCollection roomItemCollection)
+    {
+        gameWinChecker.SetLevelData(levelData.ToHashSet());
+        instructionsText.text = new LevelGenerator().GenerateLevelText(levelData);
+
+        filterController.SetColorlessFactor(0);
+        filterController.SetClearColor();
+
+        // Migrate items to new scene and then reference new collection.
+        foreach (var item in previousRoomItemCollection)
+        {
+            roomItemCollection.Add(item);
+        }
+
+        previousRoomItemCollection = roomItemCollection;
+    }
+}
